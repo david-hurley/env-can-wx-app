0d130dd2324ca49d0fc71a4f5ae812e0b34405f7
david-hurley <hurleyldave@gmail.com>
2019-11-23 23:45:30 -0800

    change server

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import dash
+import os
 
 app = dash.Dash(__name__)
-server = app.server
+server=app.server
+server.secret_key = os.environ.get('secret_key', 'secret')
+app.config.suppress_callback_exceptions = True
