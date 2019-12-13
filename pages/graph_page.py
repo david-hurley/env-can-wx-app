2d05809f4292b03e4c3a5aa85cd04abe319a5352
david-hurley <hurleyldave@gmail.com>
2019-12-12 19:38:21 -0800

    construction page

--- pages/graph_page.py
+++ pages/graph_page.py
@@ -1,28 +1,9 @@
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
-
-from app import app
 
 layout = html.Div([
     html.H3('Graph Page'),
     html.H6('Under Construction'),
     dcc.Link('Go To Home Page', href='/pages/home_page'),
-    html.Button('Click', id='button'),
-    html.Label(id='test', children='HERE')
 ])
-
-@app.callback(
-    Output(component_id='test', component_property='children'),
-    [Input(component_id='button', component_property='n_clicks')],
-    [State(component_id='create-data', component_property='data')]
-)
-def plotting(click, data):
-    print(data)
-    if click:
-        d = 'THERE'
-    else:
-        d = 'THAR'
-
-    return d
 
