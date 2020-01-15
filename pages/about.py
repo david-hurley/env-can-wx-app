8fb4555b1102ec61dfacbe158e2502c5e726249a
david-hurley <hurleyldave@gmail.com>
2020-01-15 14:36:27 -0800

    updated about page

--- pages/about.py
+++ pages/about.py
@@ -10,24 +10,57 @@ layout = html.Div([
             dcc.Link('Home Page', href='/pages/home_page')
         ], style={'textAlign': 'right', 'display': 'inline-block', 'margin-right': '2rem', 'font-size': '20px'})
     ], className='twelve columns', style={'background': '#DCDCDC', 'border': '2px black solid', 'display': 'flex'}),
+
     html.Div([
-        html.H4("What is Weather History Canada?", style={'font-weight': 'bold'}),
-        html.Label(['Weather History Canada is an open source web application for accessing historical weather data from '
-                   'Environment and Climate Change Canada maintained weather stations. Presently, historical weather data'
-                    'is accessed through the official Environment and Climate Change Canada',
-                    html.A(' historical data portal', href='https://climate.weather.gc.ca/historical_data/search_historic_data_e.html'),
-                    '. However, the current portal lacks a map search feature and only allows for downloading of data in '
-                    'monthly (for hourly data) and yearly (for daily and monthly data) increments. This means to download'
-                    '20 years of hourly data a user would need to download 240 seperate files and then combine them!']),
-        html.H4("Who is Weather History Canada for?", style={'font-weight': 'bold'}),
-        html.Label('Weather History Canada is for anyone looking to access and visualize historical weather data FAST! As'
-                   'someone with a background in meteorology and experience in environmental consulting services, I know '
-                   'how important it is to have quick access to historical weather data. Some users may be academics looking '
-                   'for additional datasets to support environmental modelling, others may be consultants looking to calculate design '
-                   'wind and flood conditions, and some might just have an interest in climate data. Regardless of the background, '
-                   'Weather History Canada is built to assist all users with accessing and viewing data quickly. '),
-        html.H4("Where did Weather History Canada come from?", style={'font-weight': 'bold'}),
-        html.Label('Weather History Canada was born out of a desire to '),
-        html.H4("Contact Weather History Canada", style={'font-weight': 'bold'})
-    ], className='six columns')
+        html.Div([
+
+            html.H4("What is Weather History Canada?", style={'font-weight': 'bold'}),
+            html.Label(['Weather History Canada is an open source web application tool for accessing historical weather data '
+                        'from over 8000 active and inactive Environment and Climate Change Canada (ECCC) maintained weather stations '
+                        'going as far back as 1840. With a few clicks of the mouse you can search, download, and visualize '
+                        'hourly, daily, and monthly ECCC weather data for any desired record length. The goal of Weather History Canada '
+                        'is to better the user experience by making it easier and quicker to download and interpret large amounts '
+                        'of historical weather data.']),
+
+            html.H4("Why is Weather History Canada needed?", style={'font-weight': 'bold'}),
+            html.Label(['Presently, historical weather data is accessed through the official ECCC ',
+                        html.A('historical data portal', href='https://climate.weather.gc.ca/historical_data/search_historic_data_e.html'),
+                        '. While ECCC does an excellent job of collecting and hosting the data the portal itself lacks certain search '
+                        'and download features. For example, since there is no way to visualize weather station locations on a map the '
+                        'user needs to know the station they are looking for in advance. The portal also limits downloading of hourly, '
+                        'monthly, and yearly weather data to small time frames. In the case of hourly weather data the portal limits downloads '
+                        'to one month increments meaning a user needing 10 years of hourly data from a select station would need to download 120 separate '
+                        'files. Additionally, the portal has no way to visualize the data once downloaded. All of this combined makes the user '
+                        'experience of searching, downloading, and visualizing data arduous.']),
+
+
+            html.H4("Who is Weather History Canada for?", style={'font-weight': 'bold'}),
+            html.Label('Weather History Canada is for anyone looking to download and visualize historical weather data '
+                       'from ECCC FAST! Whether you are an academic, industry professional, or outdoor enthusiast this site '
+                       'will allow you to begin making decisions with regards to historical weather data without the previous overhead.'),
+
+            html.H4("Where did Weather History Canada come from?", style={'font-weight': 'bold'}),
+            html.Label('Weather History Canada was born out of a desire to make the process of searching for, downloading, and visualizing'
+                       'historical Canadian weather data faster and easier. As someone with a background in meteorology and experience in the environmental '
+                       'consulting services, I know how important it is to have quick access to historical weather data when making operational '
+                       'decisions. However, I often found myself and co-workers spending multiple hours downloading weather station data from the ECCC'
+                       'portal, combining the datasets, and visualizing the data only to realize at the end that we needed something different and had to'
+                       'start over. Eventually, I became frustrated with the current system and as it goes "necessity is the mother of invention" so Weather'
+                       'History Canada was born.'),
+
+            html.H4("Limitations of Weather History Canada", style={'font-weight': 'bold'}),
+            html.Label('Weather History Canada is a tool for more quickly downloading and visualizing weather data from ECCC '
+                       'maintained weather stations. Weather History Canada makes no warranty, express or implied, and assumes'
+                       'no liability with respect to the use of weather data obtained from this site. Weather History Canada '
+                       'provides no assurance as to the quality or completeness of ECCC maintained weather data sets.')
+
+        ], className='seven columns', style={'margin-left': '1rem'}),
+
+        html.Div([
+            html.H6("Contact Weather History Canada", style={'font-weight': 'bold'}),
+            html.Label('Weather History Canada is continuously looking to improve the user experience and welcomes all comments/suggestions and questions.'),
+            html.Label(['Email: ', html.A('weatherhistorycanada@gmail.com', href='weatherhistorycanada@gmail.com')])
+        ], className='four columns', style={'margin-top': '2rem', 'margin-left': '13rem', 'border': '2px black solid', 'text-align': 'center'})
+
+    ], className='row')
 ])
