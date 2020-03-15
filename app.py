ea760208d7e081ff07644e498268956302b717ce
david-hurley <hurleyldave@gmail.com>
2020-03-15 14:27:21 -0700

    chage title on tab

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ app = dash.Dash(__name__)
 server=app.server
 server.secret_key = os.environ.get('secret_key', 'secret')
 app.config.suppress_callback_exceptions = True
+app.title = 'Weather History Canada'
